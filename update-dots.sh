72124ef3c4aec52f43078f24e407b593e71615cf
hetav21 <shahhetav2106@gmail.com>
2024-11-03 18:09:01 +0530

    auto commit

--- update-dots.sh
+++ update-dots.sh
@@ -2,8 +2,11 @@
 
 ### This scripts pushes the dots using git
 
+# Enter the path to dotfiles
 PATH_TO_DOTFILES="/home/hetav/dotfiles/"
 
-git -C ${PATH_TO_DOTFILES} add .
-git -C ${PATH_TO_DOTFILES} commit -m "auto commit"
-git -C ${PATH_TO_DOTFILES} push
+# git add, commit and push from the specified path
+sudo -u root git -C ${PATH_TO_DOTFILES} add . | bash
+sudo -u root git -C ${PATH_TO_DOTFILES} commit -m "auto commit" | bash
+sudo -u root git -C ${PATH_TO_DOTFILES} push | bash
+
