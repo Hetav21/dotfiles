65923a5f195f079cc0f8267355b7687fc0ee341f
hetav21 <shahhetav2106@gmail.com>
2024-11-03 18:11:18 +0530

    auto commit

--- update-dots.sh
+++ update-dots.sh
@@ -6,6 +6,7 @@
 PATH_TO_DOTFILES="/home/hetav/dotfiles/"
 
 # git add, commit and push from the specified path
+### changing user to root as the directory is owned by root
 sudo -u root git -C ${PATH_TO_DOTFILES} add . | bash
 sudo -u root git -C ${PATH_TO_DOTFILES} commit -m "auto commit" | bash
 sudo -u root git -C ${PATH_TO_DOTFILES} push | bash
