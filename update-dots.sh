2a98359011c8af866bccea94b39a42025c9a9963
hetav21 <shahhetav2106@gmail.com>
2024-11-05 01:22:44 +0530

    auto commit

--- update-dots.sh
+++ update-dots.sh
@@ -5,8 +5,11 @@
 # Enter the path to dotfiles
 PATH_TO_DOTFILES="/home/hetav/dotfiles/"
 
-# git add, commit and push from the specified path
 ### changing user to root as the directory is owned by root
+# Copying portage files from etc into local directory
+sudo -u root cp -r /etc/portage ./
+
+# git add, commit and push from the specified path
 sudo -u root git -C ${PATH_TO_DOTFILES} add . | bash
 sudo -u root git -C ${PATH_TO_DOTFILES} commit -m "auto commit" | bash
 sudo -u root git -C ${PATH_TO_DOTFILES} push | bash
