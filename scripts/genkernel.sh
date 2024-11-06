8ac0e48f8205383a82eeaec527dbfccb892c578c
hetav21 <shahhetav2106@gmail.com>
2024-11-06 13:44:15 +0530

    auto commit

--- scripts/genkernel.sh
+++ scripts/genkernel.sh
@@ -5,7 +5,7 @@
 ### along with re-emerging nvidia-drivers
 
 # Changes according to system
-DOTFILES_DIRECTORY="/home/hetav/dotfiles/"
+DOTFILES_DIRECTORY="/home/hetav/dotfiles"
 
 # Changes according to eselect kernel set <>
 KERNEL_DIRECTORY="/usr/src/linux"
@@ -15,7 +15,8 @@ cp ${DOTFILES_DIRECTORY}/kernel-config/current-config /usr/src/linux/.config
 
 # Make Commands
 make -C ${KERNEL_DIRECTORY} clean
-make -C ${KERNEL_DIRECTORY} -j 8
+JOBS=`expr $(nproc) / 2` # Defining no of jobs in make
+make -C ${KERNEL_DIRECTORY} -j ${JOBS}
 make -C ${KERNEL_DIRECTORY} modules_install
 make -C ${KERNEL_DIRECTORY} install
 
