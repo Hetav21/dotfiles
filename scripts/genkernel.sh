194f6d2e7c0cc62c58595733aa0d1276ae31feca
hetav21 <shahhetav2106@gmail.com>
2024-11-07 22:37:08 +0530

    Updating script to generate kernel

--- scripts/genkernel.sh
+++ scripts/genkernel.sh
@@ -2,23 +2,29 @@
 
 ### This scripts copies the existing kernel configuration and
 ### starts the kernel compilation and install
-### along with re-emerging nvidia-drivers
+### along with re-emerging external modules
 
 # Changes according to system
 DOTFILES_DIRECTORY="/home/hetav/dotfiles"
 
-# Changes according to eselect kernel set <>
+# Changes according to eselect-kernel script // symlink use flag
 KERNEL_DIRECTORY="/usr/src/linux"
 
 # Copying Current Kernel Config
 cp ${DOTFILES_DIRECTORY}/kernel-config/current-config /usr/src/linux/.config
 
-# Make Commands
+# Defining no of jobs in make
+JOBS=`expr $(nproc) / 2`
+
+# Cleaning Previous Build
 make -C ${KERNEL_DIRECTORY} clean
-JOBS=`expr $(nproc) / 2` # Defining no of jobs in make
+
+# Building kernels and external modules
+make -C ${KERNEL_DIRECTORY} modules_prepare
 make -C ${KERNEL_DIRECTORY} -j ${JOBS}
+EMERGE_DEFAULT_OPTS="--quiet" emerge @module-rebuild # Emerging external modules
+
+# Installation part
 make -C ${KERNEL_DIRECTORY} modules_install
 make -C ${KERNEL_DIRECTORY} install
 
-# Emerging Nvidia Drivers
-EMERGE_DEFAULT_OPTS="" emerge nvidia-drivers
